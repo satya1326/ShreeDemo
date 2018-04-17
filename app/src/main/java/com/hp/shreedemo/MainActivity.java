2c8941f9a21ea972c79892f97456efd20d295d37
Your Name <snm1326@gmail.com>
2018-04-17 18:43:29 +0530

    Latest Changes

--- app/src/main/java/com/hp/shreedemo/MainActivity.java
+++ app/src/main/java/com/hp/shreedemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
@@ -14,6 +15,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -29,6 +31,7 @@ import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
@@ -37,10 +40,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //TODO: views initialisation
     private ImageView imageUpload;
     //TODO: this is for the image upload
+    private File mImageFile;
     private Bitmap currentImage;
     public final int GALLERY = 1;
     public final int CAMERA= 2;
     private static final String IMAGE_DIRECTORY = "/satyaImg";
+
+
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -169,11 +179,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, GALLERY);
     }
+//    private void takePhotoFromCamera() {
+//        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(intent, CAMERA);
+//    }
     private void takePhotoFromCamera() {
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, CAMERA);
-    }
 
+//        mImageFile= new File(Environment.getExternalStorageDirectory() + File.separator + "DCIM" + File.separator + "temp.png");
+//
+//        Uri tempURI = Uri.fromFile(mImageFile);
+//        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        i.putExtra(MediaStore.EXTRA_OUTPUT, tempURI);
+//        startActivityForResult(i, CAMERA);
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAMERA);
+        }
+    }
     private void openGallery() {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("image/*");
@@ -201,11 +236,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         } else if (requestCode == CAMERA) {
-            currentImage = (Bitmap) data.getExtras().get("data");
-            Log.d("pathCamera",currentImage.toString());
-            imageUpload.setImageBitmap(currentImage);
-            saveImage(currentImage);
-            Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+
+            // See code above
+            File takenPhotoUri = getPhotoFileUri(photoFileName);
+// by this point we have the camera photo on disk
+            Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+// See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+            Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 120);
+            // Configure byte output stream
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+// Compress the image further
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+// Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+            File resizedUri = getPhotoFileUri(photoFileName + "_resized");
+            File resizedFile = new File(resizedUri.getPath());
+            try {
+                resizedFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(resizedFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+// Write the bytes of the bitmap to file
+            try {
+                fos.write(bytes.toByteArray());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+//            //TODO : this is the solution to make the image not blurred
+//            // here you image has been save to the path mImageFile.
+//            Log.d("ImagePath", "Image saved to path : " + mImageFile.getAbsolutePath());
+            currentImage = BitmapFactory.decodeFile(resizedFile.getAbsolutePath());
+//
+//            //TODO: used to compress
+//            Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(currentImage, 120);
+//
+//
+             imageUpload.setImageBitmap(currentImage);
+             saveImage(currentImage);
+             Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+
+
+            //TODO: this method is previously used but it results in blurred image
+//            currentImage = (Bitmap) data.getExtras().get("data");
+//            Log.d("pathCamera",currentImage.toString());
+//            imageUpload.setImageBitmap(currentImage);
+//            saveImage(currentImage);
+//            Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
         }
     }
     private String saveImage(Bitmap thumbnail) {
@@ -233,6 +323,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             e1.printStackTrace();
         }
         return "";
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 
     //TODO: My onActivity Result
